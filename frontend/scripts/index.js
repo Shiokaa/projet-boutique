c907addda45142775cd846121a9285abed0ce8e3
Amaru Tom <tom.amaru09@gmail.com>
2025-04-03 18:38:22 +0200

    new tshirt

--- frontend/scripts/index.js
+++ frontend/scripts/index.js
@@ -1,14 +1,15 @@
 const url = "http://localhost:3000";
 const img = document.querySelector("img");
 
-let color = "purple"; // Exemple: couleur sélectionnée par l'utilisateur
+let color = "orange"; // Exemple: couleur sélectionnée par l'utilisateur
 let view = "front"; // Exemple: vue sélectionnée
 
 fetch(`${url}/tshirt`)
   .then((response) => response.json())
   .then((data) => {
-    const tshirt = data[0];
-    img.src = `${tshirt.images}_${view}_${color}.jpg`;
+    const tshirt = data[1];
+    const images = tshirt.images;
+    img.src = images[color][view];
   })
   .catch((err) => {
     console.error("Erreur !", err);
