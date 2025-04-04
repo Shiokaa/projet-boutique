552ba2cde1b53b82178b11c77bfaabf0637edbdf
Shioka <tom.amaru09@gmail.com>
2025-04-04 17:30:36 +0200

    off screen search bar add

--- frontend/assets/scripts/header.js
+++ frontend/assets/scripts/header.js
@@ -2,9 +2,19 @@ const offScreenMenu = document.querySelector(".off-screen-menu");
 const hamburgerMenu = document.querySelector(".hamburger-menu");
 const menuText = document.querySelector(".menu-text-span");
 const overlay = document.querySelector(".overlay");
+const offScreenSearchBar = document.querySelector(".off-screen-search-bar");
+const searchBar = document.querySelector(".search-bar");
+const closeIcon = document.querySelector(".close-icon");
 
 hamburgerMenu.addEventListener("click", addActive);
 overlay.addEventListener("click", removeActive);
+
+searchBar.addEventListener("click", () => {
+  offScreenSearchBar.classList.add("active");
+});
+closeIcon.addEventListener("click", () => {
+  offScreenSearchBar.classList.remove("active");
+});
 
 function addActive() {
   offScreenMenu.classList.toggle("active");
@@ -19,4 +29,3 @@ function removeActive() {
   menuText.classList.remove("active");
   overlay.classList.remove("active");
 }
- 
